ea5df1d4938e85e0165452597de258f128d8d158
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-08-19 13:29:47 +0530

    Update Linked List Cycle II.cpp

--- Linklist/Linked List Cycle II.cpp
+++ Linklist/Linked List Cycle II.cpp
@@ -1,1 +1,30 @@
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(head==NULL || head->next==NULL)
+        {
+            return NULL;
+            
+        }
+        ListNode * slow= head;
+        ListNode * fast=head;
+        ListNode * curr=head;
 
+        while(fast->next!=NULL && fast->next->next!=NULL)
+        {
+           fast= fast->next->next;
+           slow= slow->next;
+           if(slow==fast)
+           {
+               while(slow!=curr)
+               {
+                   slow=slow->next;
+                   curr=curr->next;
+
+               }
+               return curr;
+           }
+        }
+        return NULL;
+    }
+};
