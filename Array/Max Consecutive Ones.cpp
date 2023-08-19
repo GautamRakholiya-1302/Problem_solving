2d8e40b2bee4614bf144c55873e3c6f867576eea
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-08-19 16:31:33 +0530

    Update Max Consecutive Ones.cpp

--- Array/Max Consecutive Ones.cpp
+++ Array/Max Consecutive Ones.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int n=nums.size();
+        int count =0;
+        int ans=0;
 
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==1)
+            {
+                ans=max(++count,ans);
+            }
+            else
+            {
+                count=0;
+            }
+        }
+        return ans;
+
+        
+    }
+};
