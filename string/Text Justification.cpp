37a9c8fb640a040d2d48b0f1310e5ccb8f5e11f1
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-08-24 21:44:32 +0530

    Update Text Justification.cpp

--- string/Text Justification.cpp
+++ string/Text Justification.cpp
@@ -1,1 +1,55 @@
+class Solution {
+public:
+    vector<string> fullJustify(vector<string>& words, int maxWidth) {
+        vector<string> ans;
+        int n = words.size();
+        int i = 0;
 
+        while (i < n)
+        {
+            int len = 0;
+            int start = i;
+
+            while (i < n and len + words[i].length() + (i - start) <= maxWidth) 
+            {
+                len += words[i].length();
+                i++;
+            }
+
+            int numWords = i - start;
+
+            string line = words[start];
+            int spaces = maxWidth - len;
+
+            if (numWords > 1 and i < n) 
+            {  
+                int spacesPerWord = spaces / (numWords - 1);
+                int extraSpaces = spaces % (numWords - 1);
+
+                for (int j = start + 1; j < i; j++) 
+                {
+                    line.append(spacesPerWord, ' ');
+
+                    if (extraSpaces > 0) {
+                        line.append(" ");
+                        extraSpaces--;
+                    }
+
+                    line.append(words[j]);
+                }
+            }
+            else 
+            {   
+                for (int j = start + 1; j < i; j++) 
+                {
+                    line.append(" ");
+                    line.append(words[j]);
+                }
+                line.append(maxWidth - line.length(), ' ');
+            }
+            ans.push_back(line);
+        }
+
+        return ans;
+    }
+};
