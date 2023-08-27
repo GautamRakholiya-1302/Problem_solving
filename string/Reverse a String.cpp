500b0544f10fa5c21fb477b827a63c16490ec3b6
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-08-27 11:49:40 +0530

    Create Reverse a String.cpp

---
+++ string/Reverse a String.cpp
@@ -0,0 +1,12 @@
+class Solution
+{
+    public:
+    string reverseWord(string str)
+    {
+        int i=0,j=str.size()-1;
+    while(i<j){
+        swap(str[i++],str[j--]);
+    }
+    return str;
+    }
+};
