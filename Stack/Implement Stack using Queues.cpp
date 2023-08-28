c22b3b0267fa484bebdac78bf560a89005458c69
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-08-28 11:42:55 +0530

    Update Implement Stack using Queues.cpp

--- Stack/Implement Stack using Queues.cpp
+++ Stack/Implement Stack using Queues.cpp
@@ -1,1 +1,43 @@
+class MyStack {
+public:
+    queue<int>q;
+    
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q.push(x);
 
+        
+    }
+    
+    int pop() {
+        int size=q.size();
+        while(--size)
+        {
+            int x=q.front();
+            q.pop();
+            q.push(x);
+
+        }
+        int ans=q.front();
+        q.pop();
+
+        return ans;
+
+        
+    }
+    
+    int top() {
+        return q.back();
+
+        
+    }
+    
+    bool empty() {
+        return q.size()==0;
+
+        
+    }
+};
