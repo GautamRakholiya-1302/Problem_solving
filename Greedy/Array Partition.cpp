6cb0cd700f74343eafa27e719449e33917466eb1
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-09-02 18:43:03 +0530

    Update Array Partition.cpp

--- Greedy/Array Partition.cpp
+++ Greedy/Array Partition.cpp
@@ -1,1 +1,18 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int maxi=0;
 
+        int n=nums.size();
+
+        for(int i=0;i<n;i=i+2)
+        {
+            maxi=maxi+min(nums[i],nums[i+1]);
+
+        }
+        return maxi;
+
+        
+    }
+};
