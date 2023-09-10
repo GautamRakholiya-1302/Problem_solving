8c119b09e7abec940215472172eabf8fdb4cf307
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-09-10 16:21:39 +0530

    Update Insert a node in a BST.cpp

--- Binary Search Tree/Insert a node in a BST.cpp
+++ Binary Search Tree/Insert a node in a BST.cpp
@@ -26,31 +26,31 @@ class Solution
 
 
 // Second Approch
-class Solution
-{
-    public:
-        Node* insert(Node* node, int data) {
+// class Solution
+// {
+//     public:
+//         Node* insert(Node* node, int data) {
         
-        Node* temp = node;
-        Node* newNode = new Node(data);
-        Node* t = node;
-        while(temp) {
-            if(temp->data < data && temp->right) {
-                temp = temp->right;
-            } else if(temp->data > data && temp->left) {
-                temp = temp->left;
-            } else {
-                break;
-            }
-        }
-        if(temp->data == data) {
-            return node;
-        } 
-        if(temp->data < data) {
-            temp->right = newNode;
-        } else {
-            temp->left = newNode;
-        }
-        return node;
-    }
-};
+//         Node* temp = node;
+//         Node* newNode = new Node(data);
+//         Node* t = node;
+//         while(temp) {
+//             if(temp->data < data && temp->right) {
+//                 temp = temp->right;
+//             } else if(temp->data > data && temp->left) {
+//                 temp = temp->left;
+//             } else {
+//                 break;
+//             }
+//         }
+//         if(temp->data == data) {
+//             return node;
+//         } 
+//         if(temp->data < data) {
+//             temp->right = newNode;
+//         } else {
+//             temp->left = newNode;
+//         }
+//         return node;
+//     }
+// };
