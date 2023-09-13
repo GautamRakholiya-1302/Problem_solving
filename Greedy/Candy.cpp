6763265873dda74a3d75ceb784b6247ab3ff1699
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-09-13 17:48:13 +0530

    Update Candy.cpp

--- Greedy/Candy.cpp
+++ Greedy/Candy.cpp
@@ -1,1 +1,14 @@
-
+int n = ratings.size();
+        vector<int> candy(n,1);
+        
+        for(int i=1; i<n; i++){
+            if(ratings[i] > ratings[i-1])
+                candy[i] = candy[i-1]+1;
+        }
+        
+        for(int i=n-2; i>=0; i--){
+            if(ratings[i] > ratings[i+1] && candy[i]<=candy[i+1])
+                candy[i] = candy[i+1]+1;
+        }
+        
+        return accumulate(candy.begin(), candy.end(), 0);
