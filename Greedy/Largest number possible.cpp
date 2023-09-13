88e93f35a015841b3f2325afc59bd6c5d5210444
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-09-13 17:42:18 +0530

    Update Largest number possible.cpp

--- Greedy/Largest number possible.cpp
+++ Greedy/Largest number possible.cpp
@@ -1,1 +1,16 @@
-
+ string findLargest(int N, int S){
+       string s;
+        if(S==0 && N == 1) return "0";
+        if(S==0 && N>1) return "-1";
+        if(N*9 < S) return "-1";
+        while(S>0){
+            int i = S >= 9 ? 9 : S;
+            s += i + '0';
+            S-=i;
+        }
+        while(s.size() != N){
+            s+='0';
+        }
+        
+        return s;
+    }
