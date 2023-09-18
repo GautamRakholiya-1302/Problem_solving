6e42c69d9f46fa348d23b7fc5d9396c4219205fc
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-09-18 23:14:09 +0530

    Update Power of 2.cpp

--- Array/Power of 2.cpp
+++ Array/Power of 2.cpp
@@ -1,1 +1,18 @@
-
+class Solution{
+    public:
+    bool isPowerofTwo(long long n){
+        
+        int c = 0;
+        while (n > 0)
+        {
+            if ((n & 1) == 1)
+                c++;
+            n /= 2;
+        }
+        if (c != 1)
+            return false;
+        else
+            return true;
+        
+    }
+};
