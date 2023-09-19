c44673ab81249b35908a7a351a59eede1a44b1a0
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-09-19 23:20:23 +0530

    Update Find first set bit.cpp

--- Bit Manipulation/Find first set bit.cpp
+++ Bit Manipulation/Find first set bit.cpp
@@ -1,1 +1,14 @@
-
+class Solution
+{
+    public:
+    unsigned int getFirstSetBit(int n)
+    {
+         int count = 1;
+        while(n  > 0){
+            if(n & 1) return count;
+            n = n >> 1;
+            count++;
+        }
+        return 0;// Your code here
+    }
+};
