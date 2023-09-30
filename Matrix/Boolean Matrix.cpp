3791e9d92564f0d40c1d59aa3aa9b70cb0d7d759
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-10-01 00:01:05 +0530

    Update Boolean Matrix.cpp

--- Matrix/Boolean Matrix.cpp
+++ Matrix/Boolean Matrix.cpp
@@ -1,1 +1,31 @@
+class Solution
+{   
+    public:
+    
+    void booleanMatrix(vector<vector<int> > &matrix)
+    {
+        unordered_set<int>row;
+        unordered_set<int>col;
+        int n=matrix.size();
+        int m=matrix[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                 if(matrix[i][j] == 1){
+                     row.insert(i);
+                     col.insert(j);
+                 }
+            }
+        }
+        for(auto i:row){
+            for(int j=0;j<m;j++){
+                  matrix[i][j]=1;
+            }
+        }
+        for(auto j:col){
+            for(int i=0;i<n;i++){
+                  matrix[i][j]=1;
+            }
+        }
+    }
+};
 
