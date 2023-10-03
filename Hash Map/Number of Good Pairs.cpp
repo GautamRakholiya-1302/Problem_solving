448d8180becacb489f3c768c72088ef3c88df589
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-10-03 21:29:35 +0530

    Update Number of Good Pairs.cpp

--- Hash Map/Number of Good Pairs.cpp
+++ Hash Map/Number of Good Pairs.cpp
@@ -1,1 +1,18 @@
 
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        vector<int>count(101,0);
+        int n=nums.size();
+        int ans=0;
+
+
+        for(auto &num:nums)
+        {
+            ans=ans+count[num];
+            count[num]++;
+        }
+        return ans;
+
+    }
+};
