140fbd65dfa04b205d167e63e12f679995005c2e
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-10-03 21:30:59 +0530

    Update Column name from a given column number.cpp

--- string/Column name from a given column number.cpp
+++ string/Column name from a given column number.cpp
@@ -1,1 +1,20 @@
+class Solution{
+    public:
+    string colName (long long int n)
+    {
+        string ans="";
+          while(n>0)
+          {
+              int pos=(n-1)%26;
+              char ch='A'+pos;
+              ans= ch+ans;
+              n=(n-1)/26;
+          }
+        
+          return ans;
+    }
+};
+
+
+
 
