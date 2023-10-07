0359b4ccd858d9a7fff82c031a6eaef78d30956b
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-10-07 22:53:49 +0530

    Update Pairwise swap elements of a linked list.cpp

--- Linklist/Pairwise swap elements of a linked list.cpp
+++ Linklist/Pairwise swap elements of a linked list.cpp
@@ -1,1 +1,14 @@
-
+class Solution
+{
+    public:
+   Node* pairWiseSwap(struct Node* head)
+    {
+        if(!head || !head->next)
+            return head;
+            
+        Node* sec = head->next;
+        head->next = pairWiseSwap(sec->next);
+        sec->next = head;
+        return sec;
+    }
+};
