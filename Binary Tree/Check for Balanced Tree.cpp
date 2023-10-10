acca20cd74c1df8a9e4adf129b2cd7a9c1930da4
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-10-11 00:49:07 +0530

    Update Check for Balanced Tree.cpp

--- Binary Tree/Check for Balanced Tree.cpp
+++ Binary Tree/Check for Balanced Tree.cpp
@@ -1,1 +1,15 @@
-
+int ans=1;
+    int  solve(Node*  root)
+   {
+       if(root==NULL) return 0;
+       int l=solve(root->left);
+       int r=solve(root->right);
+       if(abs(l-r)>1) ans=0;
+       return max(l,r)+1;
+   }
+   
+   bool isBalanced(Node *root)
+   {
+     solve(root);
+     return ans;
+     
