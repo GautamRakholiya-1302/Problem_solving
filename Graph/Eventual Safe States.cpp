cf7bb2349a88b7e576a77e6287fa61937033f732
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-10-18 22:43:44 +0530

    Update Eventual Safe States.cpp

--- Graph/Eventual Safe States.cpp
+++ Graph/Eventual Safe States.cpp
@@ -1,1 +1,22 @@
-
+class Solution {
+  public:
+     bool check(int node,vector<int> adj[],vector<int> &vis){
+        if(vis[node]) return false;
+        vis[node] = 1;
+        for(auto it : adj[node]){
+            if(check(it,adj,vis)==false) return false;
+        }
+        vis[node]=0;
+        return true;
+    }
+  public:
+    vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
+        vector<int> ans;
+        vector<int> vis(V,0);
+        for(int i=0; i<V; i++){
+            if(check(i,adj,vis)) ans.push_back(i);
+        }
+            
+        return ans;
+    }
+};
