3a5b3498e3110d33bb7f4174b4e6072c7305bf9e
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-10-21 17:38:20 +0530

    Update Sum of all divisors from 1 to n.cpp

--- Array/Sum of all divisors from 1 to n.cpp
+++ Array/Sum of all divisors from 1 to n.cpp
@@ -1,1 +1,14 @@
-
+class Solution
+{
+public:
+    long long sumOfDivisors(int N)
+    {
+   long long sum=0;
+        
+        for(int i=1 ; i<=N ; i++)
+         {
+             sum+= i*(N/i) ;
+         }
+         
+         return sum;    }
+};
