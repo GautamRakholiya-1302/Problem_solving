4775d6a60c8d9692a0f85ed3b5e1d89fe7a84109
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-10-26 21:15:25 +0530

    Update Minimum Operations.cpp

--- Greedy/Minimum Operations.cpp
+++ Greedy/Minimum Operations.cpp
@@ -1,1 +1,16 @@
-
+class Solution
+{
+  public:
+    int minOperation(int n)
+    {
+       int c=0;
+        while(n){
+            if(n%2!=0)  
+                n--;  
+            else
+                n/=2;
+            c++;
+        }
+        return c; //code here.
+    }
+};
