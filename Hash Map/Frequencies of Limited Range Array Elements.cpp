56c9ff964a1875cdfa350a89f743b1e5c00eaf60
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-11-01 18:12:49 +0530

    Create Frequencies of Limited Range Array Elements.cpp

---
+++ Hash Map/Frequencies of Limited Range Array Elements.cpp
@@ -0,0 +1,21 @@
+class Solution{
+    public:
+    //Function to count the frequency of all elements from 1 to N in the array.
+    void frequencyCount(vector<int>& arr,int N, int P)
+    { 
+        for(int i = 0; i < N; i++) {
+            if(arr[i] > N)
+                arr[i] = 0;
+        }
+        
+        for(int i = 0; i < N; i++) {
+            if(arr[i]%(N+1) > 0)
+                arr[(arr[i]%(N+1))-1] += (N+1);
+        }
+        
+        for(int i = 0; i < N; i++) {
+            arr[i] /= (N+1);
+        }
+    
+    }
+};
