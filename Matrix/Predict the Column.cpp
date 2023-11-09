9f8563ab76bc6c8c71c5e90b7d0e5851d8fb5ab9
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-11-09 18:06:11 +0530

    Update Predict the Column.cpp

--- Matrix/Predict the Column.cpp
+++ Matrix/Predict the Column.cpp
@@ -1,1 +1,27 @@
-
+class Solution{
+    public:
+  
+    int columnWithMaxZeros(vector<vector<int>>arr,int N){
+        
+         int max=-1;
+       int ans=-1;
+       for(int i=0;i<N;i++)
+       {
+           int cnt=0;
+           for(int j=0;j<N;j++)
+           {
+               if(arr[j][i]==0)
+               cnt++;
+           }
+           if(max<cnt)
+           {
+               max=cnt;
+               ans=i;
+           }
+       }
+       if(max==0)
+       return -1;
+       return ans;
+        
+        
+    }
