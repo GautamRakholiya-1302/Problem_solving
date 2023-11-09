a15d9df863972d98260842ff4e01aaed46246c8d
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-11-09 18:08:01 +0530

    Update Count Number of Homogenous Substrings.cpp

--- string/Count Number of Homogenous Substrings.cpp
+++ string/Count Number of Homogenous Substrings.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    int countHomogenous(string s) {
+        int  count=0,ans=0;
 
+        for(int i=0;i<s.size();++i)
+        {
+            if(i>0 && s[i-1]==s[i])
+            {
+                ++count;
+            }
+            else
+            {
+                count=1;
+
+            }
+            ans=(count+ans)%1000000007;
+        }
+        return ans;
+
+    }
+};
