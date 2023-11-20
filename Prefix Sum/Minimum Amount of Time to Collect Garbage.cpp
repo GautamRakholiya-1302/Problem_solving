d1d11e25f798aa37b5f4cc19025c6aab8ded637b
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-11-20 21:25:48 +0530

    Update Minimum Amount of Time to Collect Garbage.cpp

--- Prefix Sum/Minimum Amount of Time to Collect Garbage.cpp
+++ Prefix Sum/Minimum Amount of Time to Collect Garbage.cpp
@@ -1,1 +1,52 @@
+class Solution {
+public:
+    int garbageCollection(vector<string>& garbage, vector<int>& travel) {
 
+        int count=0;
+        int glass=0;
+        int pep=0;
+        int met=0;
+        for(int i=0;i<garbage.size();i++)
+        {
+            for(auto it:garbage[i] )
+            {
+                if(it=='M')
+                {
+                    met=i;
+                }
+                else if(it=='P')
+                {
+                    pep=i;
+                }
+                else
+                {
+                    glass=i;
+                }
+                count++;
+            }
+
+        }
+        int ans=count;
+        int sum=0;
+
+        for(int i=0;i<travel.size();i++)
+        {
+            sum+=travel[i];
+            if(glass==i+1)
+            {
+                ans+=sum;
+            }
+            if(pep==i+1)
+            {
+                ans+=sum;
+            }
+            if(met==i+1)
+            {
+                ans+=sum;
+            }
+        }
+        return ans;
+
+        
+    }
+};
