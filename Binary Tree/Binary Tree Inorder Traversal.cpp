7660677dd547fdb020afcd6227ac5466b2299926
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-12-09 23:04:59 +0530

    Update Binary Tree Inorder Traversal.cpp

--- Binary Tree/Binary Tree Inorder Traversal.cpp
+++ Binary Tree/Binary Tree Inorder Traversal.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    void inorder(TreeNode * root,vector<int>&nodes)
+    {
+        if(!root)
+        {
+            return ;
+        }
+        inorder(root->left,nodes);
+        nodes.push_back(root->val);
+        inorder(root->right,nodes);
+    }
 
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>nodes;
+        inorder(root,nodes);
+        return nodes;
+        
+    }
+};
