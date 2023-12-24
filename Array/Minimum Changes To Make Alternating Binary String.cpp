718992997cd9c1f2fe1489ab20aca2b3ec78948c
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2023-12-24 23:51:19 +0530

    Update Minimum Changes To Make Alternating Binary String.cpp

--- Array/Minimum Changes To Make Alternating Binary String.cpp
+++ Array/Minimum Changes To Make Alternating Binary String.cpp
@@ -1,17 +1,7 @@
 class Solution {
 public:
         int minOperations(string s) {
-    //    int n=s.size();
-    //    int op[2]={0};
-    //    for(int i=0;i<n;i+=2)
-    //    {
-    //        op[s[i]=='0']++;
-    //    }
-    //    for(int i=0;i<n;i+=2)
-    //    {
-    //        op[s[i]=='1']++;
-    //    }
-    //    return min(op[0],op[1]);
+  
     int n=s.size(), op[2]={0};
         for(int i=0; i<n; i+=2)
             op[s[i]=='0']++;
