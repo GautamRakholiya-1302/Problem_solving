68a7aea3466184eef89ded65ba70e1277eb13f3a
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2024-02-07 23:15:52 +0530

    Update Sort Characters By Frequency.cpp

--- Hash Map/Sort Characters By Frequency.cpp
+++ Hash Map/Sort Characters By Frequency.cpp
@@ -1,1 +1,36 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char, int> mp;
+        vector<pair<int, char>> v;
+        string ans = "";
 
+        // count character frequency
+        for(auto ch: s){
+            mp[ch]++;
+        }
+
+        // push from map to vector 
+        for(auto i: mp){
+            v.push_back({i.second, i.first});
+        }
+        cout << "================: ";
+        for(auto i: v){
+            cout << "(((" << i.first << ", " << i.second << "))) ";
+        }
+
+        // sort the vector in decreasing order
+        sort(v.begin(), v.end(), greater<pair<int, char>>());
+
+        cout << "Contents of vector v: ";
+        for(auto i: v){
+            cout << "(" << i.first << ", " << i.second << ") ";
+        }
+        cout << endl; 
+        // add to final answer string
+        for(auto i: v){
+            while(i.first--) ans += i.second;
+        }
+        return ans;
+    }
+};
