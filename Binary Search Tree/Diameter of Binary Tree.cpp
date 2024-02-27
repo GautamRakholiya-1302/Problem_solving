c8a8e58dc8c9b7c8fd30b198e6a63d27a42463d7
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2024-02-27 21:51:49 +0530

    Update Diameter of Binary Tree.cpp

--- Binary Search Tree/Diameter of Binary Tree.cpp
+++ Binary Search Tree/Diameter of Binary Tree.cpp
@@ -1,1 +1,19 @@
+class Solution {
+public:
+    int diameter(TreeNode* root, int &temp)
+    {
+        if(root==NULL) return 0;
+        int left=diameter(root->left,temp);
+        int right=diameter(root->right,temp);
+        temp=max(temp,left+right);
+        return max(left,right)+1;
 
+    }
+    
+     int diameterOfBinaryTree(TreeNode* root) {
+        int temp=0;
+        diameter(root,temp);
+        return temp;
+
+    }
+};
