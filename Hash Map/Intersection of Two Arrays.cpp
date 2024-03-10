13a18d92578cbdb216915aba3a809bf1e32ab426
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2024-03-10 20:39:16 +0530

    Update Intersection of Two Arrays.cpp

--- Hash Map/Intersection of Two Arrays.cpp
+++ Hash Map/Intersection of Two Arrays.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        std::vector<int>ans;
+        set<int> set;
+        for(int i=0;i<nums1.size();i++)
+        {
+            for(int j=0;j<nums2.size();j++)
+            {
+                if(nums1[i]==nums2[j])
+                {
+                    set.insert(nums1[i]);
+                }
+            }
+        }
+       
+        std::copy(set.begin(), set.end(), std::back_inserter(ans));
+        return ans;
 
+
+        
+    }
+};
