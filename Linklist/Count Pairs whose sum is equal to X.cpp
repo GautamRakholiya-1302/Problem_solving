7e232cff6f03f947f848ad0e3012d009962d28e0
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2024-03-17 23:53:02 +0530

    Update Count Pairs whose sum is equal to X.cpp

--- Linklist/Count Pairs whose sum is equal to X.cpp
+++ Linklist/Count Pairs whose sum is equal to X.cpp
@@ -1,1 +1,22 @@
-
+class Solution{
+  public:
+    // your task is to complete this function
+    int countPairs(struct Node* head1, struct Node* head2, int x) {
+       map<int,int>mp;
+        while(head1!=NULL)
+        {
+            mp[head1->data]++;
+            head1=head1->next;
+        }
+        int count=0;
+        while(head2!=NULL)
+        {
+            if(mp.find(x-head2->data)!=mp.end())
+            {
+                count++;
+            }
+            head2=head2->next;
+        }
+        return count;
+    }
+};
