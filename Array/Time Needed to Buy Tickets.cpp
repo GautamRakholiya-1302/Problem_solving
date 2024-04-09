2ab8b86b16c9927ed4714fe444368a14c8e5044e
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2024-04-09 22:28:57 +0530

    Update Time Needed to Buy Tickets.cpp

--- Array/Time Needed to Buy Tickets.cpp
+++ Array/Time Needed to Buy Tickets.cpp
@@ -14,3 +14,11 @@ public:
         return total;
     }
 };
+
+
+int timeRequiredToBuy(vector<int>& t, int k) {
+    int res = 0, sz = t.size(), cap = t[k];
+    for (int i = 0; i < sz; ++i)
+        res += min(cap - (i > k), t[i]);
+    return res;
+}
