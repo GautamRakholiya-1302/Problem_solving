dd3304994c8459232bbfd982df564772b92ff866
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2024-04-12 21:42:24 +0530

    Update Trapping Rain Water.cpp

--- Stack/Trapping Rain Water.cpp
+++ Stack/Trapping Rain Water.cpp
@@ -1,1 +1,36 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n=height.size();
 
+        int left[n];
+        int right[n];
+
+        left[0]=height[0];
+        for(int i=1;i<n;i++)
+        {
+            left[i]=max(left[i-1],height[i]);
+
+        }
+        right[n-1]=height[n-1];
+        for(int j=n-2;j>=0;j--)
+        {
+            right[j]=max(right[j+1],height[j]);
+
+        }
+        int water[n];
+        for(int i=0;i<n;i++)
+        {
+            water[i]=min(left[i],right[i])-height[i];
+
+
+        }
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum=sum+water[i];
+
+        }
+        return sum;
+    }
+};
