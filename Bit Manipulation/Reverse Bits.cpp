e9be55fd46470bafbf1db44d67feddef411f06e9
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2024-04-13 23:45:49 +0530

    Update Reverse Bits.cpp

--- Bit Manipulation/Reverse Bits.cpp
+++ Bit Manipulation/Reverse Bits.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+  public:
+    long long reversedBits(long long x) {
+       long long ans = 0;
+        for(int i = 0; i < 32; i++){
+            ans <<= 1;
+            ans = (ans | (x & 1));
+            x >>= 1;
+            
+        }
+        return ans; // code here
+    }
+};
