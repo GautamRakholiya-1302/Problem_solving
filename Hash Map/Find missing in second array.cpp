cc72330d70812894b9883aa45643a92e328ec63d
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2024-04-19 23:55:38 +0530

    Update Find missing in second array.cpp

--- Hash Map/Find missing in second array.cpp
+++ Hash Map/Find missing in second array.cpp
@@ -18,41 +18,41 @@ class Solution{
 	    return ans;// Your code goes here
 	} 
 };
-vector<int>ans;
-         bool present=0;
-        sort(b,b+5);
-      for(int i=0;i<6;i++)
-      {
-           int num=a[i];
-           int s=0;
-           int e=5-1;
-           int mid=(s+e)/2;
-           while(s<=e)
-           {
-                present=0;
-               if(b[mid]==num)
-              {
-                  present=1;
-                  cout<<"number present"<<num<<"\n";
-                  break;
-              }
-              else if(num>b[mid])
-              {
-                  s=mid+1;
-              }
-              else
-              {
-                  e=mid-1;
-              }
-              mid=(s+e)/2;
-           }
-           if(present==0)
-           {
-              ans.push_back(num);
-           }
-      }
-      for(auto it:ans)
-      {
-        cout<<it<<" ";
-      }
-  }
+// vector<int>ans;
+//          bool present=0;
+//         sort(b,b+5);
+//       for(int i=0;i<6;i++)
+//       {
+//            int num=a[i];
+//            int s=0;
+//            int e=5-1;
+//            int mid=(s+e)/2;
+//            while(s<=e)
+//            {
+//                 present=0;
+//                if(b[mid]==num)
+//               {
+//                   present=1;
+//                   cout<<"number present"<<num<<"\n";
+//                   break;
+//               }
+//               else if(num>b[mid])
+//               {
+//                   s=mid+1;
+//               }
+//               else
+//               {
+//                   e=mid-1;
+//               }
+//               mid=(s+e)/2;
+//            }
+//            if(present==0)
+//            {
+//               ans.push_back(num);
+//            }
+//       }
+//       for(auto it:ans)
+//       {
+//         cout<<it<<" ";
+//       }
+//   }
