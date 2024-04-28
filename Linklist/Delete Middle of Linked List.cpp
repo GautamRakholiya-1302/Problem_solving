93c0fedc8ecf29d226ae6413c98a55311977a288
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2024-04-28 23:05:14 +0530

    Update Delete Middle of Linked List.cpp

--- Linklist/Delete Middle of Linked List.cpp
+++ Linklist/Delete Middle of Linked List.cpp
@@ -1,1 +1,20 @@
-
+class Solution{
+    public:
+    Node* deleteMid(Node* head)
+    {
+        if(head == NULL || head->next == NULL){
+            return NULL;
+        }
+        Node* temp = head;
+        unordered_map<int, Node*> mp;
+        int n = 0;
+        while(temp != NULL){
+            n++;
+            mp[n] = temp;
+            temp = temp->next;
+        }
+        n = n/2;
+        mp[n]->next = mp[n+2]; 
+        return head; // Your Code Here
+    }
+};
