c4d0c1b1d39e8f887b7b69d637808d229c107efe
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2024-04-29 23:20:11 +0530

    Update Remove every kth node.cpp

--- Linklist/Remove every kth node.cpp
+++ Linklist/Remove every kth node.cpp
@@ -1,1 +1,28 @@
+class Solution {
+    public:
+    Node* deleteK(Node *head,int K){
+      if(head==NULL || K==1){
+            return NULL;
+        }
+        
+      Node*prev=NULL;
+      Node*temp=head;
+      int i=0;
+      while(temp!=NULL){
+           i++;
+              if(i==K){
+                  prev->next=temp->next;
+                  temp->next=NULL;
+                  temp=prev->next;
+                  i=0;
+              }else{
+                  prev=temp;
+                  temp=temp->next;
 
+              }
+          
+      }
+      return head;
+       //Your code here
+    }
+};
