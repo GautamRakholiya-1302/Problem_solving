40d88c27ac973df366f7a1e699f74dc932adc8e1
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2024-05-08 23:36:09 +0530

    Create Root to Leaf Paths.cpp

---
+++ Binary Tree/Root to Leaf Paths.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+   void solve(Node* root,vector<vector<int>> &ans, vector<int> path){
+        if(!root->left && !root->right){
+            path.push_back(root->data);
+            ans.push_back(path);
+            return;
+        }
+        path.push_back(root->data);
+        if(root->left){
+            solve(root->left,ans,path);
+        }
+        if(root->right){
+            solve(root->right,ans,path);
+        }
+    }
+    vector<vector<int>> Paths(Node* root) {
+        vector<vector<int>> ans;
+        vector<int> path;
+        solve(root,ans,path);
+        return ans;
+    }
+};
