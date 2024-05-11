d33a740916681df89bcfdac76a26f9d5b1acdc81
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2024-05-12 00:00:12 +0530

    Update Juggler Sequence.cpp

--- Recursion/Juggler Sequence.cpp
+++ Recursion/Juggler Sequence.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+  public:
+    vector<long long> jugglerSequence(long long n) {
+        vector<long long> res;
+        
+        res.push_back(n);
+        while(n!=1){
+            if(n%2==0)
+            n=sqrt(n);
+            else
+            n=pow(sqrt(n), 3);
+            
+            res.push_back(n);
+        }
+       
+        return res; // code here
+    }
+};
