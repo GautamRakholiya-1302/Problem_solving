4d79e61a2fc5e94b8567ad43202061096855877e
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2024-05-18 22:11:57 +0530

    Update Find the Highest number.cpp

--- Binary Search/Find the Highest number.cpp
+++ Binary Search/Find the Highest number.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    int findPeakElement(vector<int>& a) 
+    {
+        int large=a[0];
+        
+        for(int x=0;x<a.size();x++)
+        {
+            if(a[x]>=large)
+            {
+                large=a[x];
+            }
+        }
+        return large; // Code here.
+    }
+};
