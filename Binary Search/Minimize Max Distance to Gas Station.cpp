2ca2827b62cbf11741f4857ec1f71597fd18710a
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2024-05-22 01:51:08 +0530

    Update Minimize Max Distance to Gas Station.cpp

--- Binary Search/Minimize Max Distance to Gas Station.cpp
+++ Binary Search/Minimize Max Distance to Gas Station.cpp
@@ -1,1 +1,42 @@
-
+class Solution {
+  public:
+      bool solve(double md,vector<int>&st,int n,int k)
+  {
+      int count=0;
+      for(int i=1;i<n;i++)
+      {
+          int x=floor((st[i]-st[i-1])/md);
+          if(2*x == (double)(st[i]-st[i-1])){
+            --x;
+            }
+          count+=x;
+      }
+      if(count<=k)
+      {
+          return true;
+      }
+      return false;
+  }
+    double findSmallestMaxDist(vector<int> &st, int k) {
+        // Code here
+        int n=st.size();
+        double mini=0;
+        double maxi=st[n-1]-st[0];
+        double ans=0;
+        while((maxi-mini)>1e-7)
+        {
+            double mid=(maxi-mini)/2.0+mini;
+            if(solve(mid,st,n,k))
+            {
+                ans=mid;
+                maxi=mid;
+                
+            }
+            else
+            {   
+                mini=mid;
+            }
+        }
+        return ans;
+    }
+};
