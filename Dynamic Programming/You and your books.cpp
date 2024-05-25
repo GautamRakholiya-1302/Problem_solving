20644a65dc072c8557e9cddc707a1931e4d2f391
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2024-05-25 20:53:38 +0530

    Update You and your books.cpp

--- Dynamic Programming/You and your books.cpp
+++ Dynamic Programming/You and your books.cpp
@@ -1,1 +1,22 @@
-
+class Solution {
+  public:
+    /*You are requried to complete this method */
+    long long max_Books(int arr[], int n, int k) {
+        int l = 0, r = 0;
+        long long maxi = 0;
+        long long sum = 0;
+        while(r < n){
+            if(arr[r] > k){
+                r++;
+                l = r;
+                sum = 0;
+            }
+            else{
+                sum += arr[r];
+                r++;
+                maxi = max(maxi ,sum);
+            }
+        }
+        return maxi; // Your code here
+    }
+};
