df6c0c7d090f61c030356835604c0ce758f9345e
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2024-05-27 23:01:23 +0530

    Update Longest subsequence-1.cpp

--- Dynamic Programming/Longest subsequence-1.cpp
+++ Dynamic Programming/Longest subsequence-1.cpp
@@ -1,1 +1,14 @@
-Longest subsequence-1.cpp
+class Solution {
+      public:
+        int longestSubseq(int n, vector<int> &a) {
+               unordered_map<int,int>mp;
+            int maxi=0,b,c;
+            for(auto i:a)
+            {
+                b=i-1,c=i+1;
+                mp[i]=max(mp[b],mp[c])+1;
+                maxi=max(maxi,mp[i]);
+            }
+            return maxi;// code here
+        }
+    };
