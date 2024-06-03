216508694e32aa2a9d29554b6db3300c82e904eb
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2024-06-04 00:44:23 +0530

    Update Binary representation of next number.cpp

--- Bit Manipulation/Binary representation of next number.cpp
+++ Bit Manipulation/Binary representation of next number.cpp
@@ -1,1 +1,29 @@
-
+class Solution {
+  public:
+    string binaryNextNumber(string s) {
+        int n = s.length();
+        string ans;
+        int i = 0;
+        while(s[i] == '0')  i++;
+        while(i < n){
+            ans.push_back(s[i]);
+            i++;
+        }
+        n = ans.size();
+        if(ans.back() == '0'){
+            ans[n-1] = '1';
+            return ans;
+        }
+        for(int i = n-1; i >= 0; i--){
+            if(ans[i] == '0'){
+                ans[i] = '1';
+                return ans;
+            }
+            else{
+                ans[i] = '0';
+            }
+        }
+        ans.insert(ans.begin(), '1');
+        return ans; // code here.
+    }
+};
