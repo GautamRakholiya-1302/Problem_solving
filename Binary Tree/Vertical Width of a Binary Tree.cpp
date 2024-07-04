13bbe52b4cf691deb1f5cb5119d661ca13ec124b
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2024-07-05 00:06:54 +0530

    Create Vertical Width of a Binary Tree.cpp

---
+++ Binary Tree/Vertical Width of a Binary Tree.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    // Function to find the vertical width of a Binary Tree.
+    int mini=INT_MAX;
+int maxi=INT_MIN;
+void traversal(Node*root,int i){
+    if(root==NULL){
+        return;
+    }
+    
+    mini=min(mini,i);
+    maxi=max(maxi,i);
+    
+    traversal(root->left,i-1);
+    traversal(root->right,i+1);
+}
+
+int verticalWidth(Node* root) {
+    
+       if(root==NULL){
+           return 0;
+       }
+      
+      mini=INT_MAX;
+      maxi=INT_MIN;
+     traversal(root,0);
+     return (maxi-mini)+1;
+    }
+};
