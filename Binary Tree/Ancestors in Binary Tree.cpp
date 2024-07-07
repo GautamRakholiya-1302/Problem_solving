a48c0b7fa3f880da2b39c12e11f88d67a45ccfbf
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2024-07-07 22:48:11 +0530

    Create Ancestors in Binary Tree.cpp

---
+++ Binary Tree/Ancestors in Binary Tree.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    // Function should return all the ancestor of the target node
+    bool findanc(Node* root,int k,vector<int> &ds)
+  {
+      if(root==NULL) return false;
+      if(root->data==k) return true;
+      bool l=findanc(root->left,k,ds);
+      if(l){
+          ds.push_back(root->data);
+          return true;
+        }
+      bool r=findanc(root->right,k,ds);
+      if(r){
+          ds.push_back(root->data);
+          return true;
+        }
+      return false;
+  }
+  public:
+    vector<int> Ancestors(struct Node *root, int target) {
+
+        if(!root || root->data==target) return {};
+        vector<int> ans;
+        bool t=findanc(root,target,ans);
+        return ans;
+    }
+};
